9491fb5e947c0b053a7010f0370c3455aa5fc9dd
overtrue <anzhengchao@gmail.com>
2015-03-19 15:29:28 +0800

    Add pinyin_and_letter function

--- src/helpers.php
+++ src/helpers.php
@@ -36,3 +36,20 @@ if (! function_exists('letter')) {
 } else {
     Log::warning('There exist multiple function "letter".');
 }
+
+if (! function_exists('pinyin_and_letter')) {
+    /**
+     * Get the fist pinyin and letters of given string.
+     *
+     * @param  string $string
+     * @param  array  $setting
+     *
+     * @return string
+     */
+    function pinyin_and_letter($string, $setting = [])
+    {
+        return Pinyin::parse($string, $setting);
+    }
+} else {
+    Log::warning('There exist multiple function "pinyin_and_letter".');
+}
