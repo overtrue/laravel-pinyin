8d4a1c1344a518f968d038ac5428a60a4aab98ae
overtrue <anzhengchao@gmail.com>
2023-02-15 16:51:29 +0800

    Laravel 10

--- src/helpers.php
+++ src/helpers.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the overtrue/laravel-pinyin.
- *
- * (c) overtrue <i@overtrue.me>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 use Illuminate\Support\Facades\Log;
 use Overtrue\Pinyin\Pinyin;
 
